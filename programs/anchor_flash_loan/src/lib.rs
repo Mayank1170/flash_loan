e44ad070d20456930b3f4ce6c7e8c9da8c2da898
Mayank1170 <agarwalmayank1170@gmail.com>
2025-11-19 11:18:24 +0530

    Added borrow and repay instructions and fixed build errorss

--- programs/anchor_flash_loan/src/lib.rs
+++ programs/anchor_flash_loan/src/lib.rs
@@ -5,10 +5,11 @@ use anchor_spl::{
 };
 
 use anchor_lang::{
-    Descriminator,
-    solana_program::sysvar::instruction::{
+    Discriminator,
+    solana_program::sysvar::instructions::{
         ID as INSTRUCTION_SYSVAR_ID,
-        load_instruction_at_checked
+        load_instruction_at_checked,
+        load_current_index_checked,
     }
 };
 
@@ -16,21 +17,73 @@ use anchor_lang::{
 declare_id!("3gJyGpTNRDTJN4f1eMNDZ8VdvRhnXGFwW2ZZB4jnGNUP");
 
 #[program]
-pub mod sountsnchor_flash_loan {
+pub mod anchor_flash_loan {
     use super::*;
 
     pub fn borrow(ctx: Context<Loan>, borrow_amount: u64) -> Result<()> {
+        require!(borrow_amount>0, ProtocolError::InvalidAmount);
+        let seeds = &[
+            b"protocol".as_ref(),
+            &[ctx.bumps.protocol],
+        ];
+        let signer_seed = &[&seeds[..]];
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer{
+                    from: ctx.accounts.protocol_ata.to_account_info(),
+                    to: ctx.accounts.borrower_ata.to_account_info(),
+                    authority: ctx.accounts.protocol.to_account_info(),
+                },
+                signer_seed
+            ),
+            borrow_amount
+        )?; 
+        let ixs = ctx.accounts.instructions.to_account_info();
+        let current_index = load_current_index_checked( &ctx.accounts.instructions)?;
+        require_eq!(current_index, 0, ProtocolError::InvalidIx); 
+        let instruction_sysvar = ixs.try_borrow_data()?;
+        let len = u16::from_le_bytes(instruction_sysvar[0..2].try_into().unwrap());
+        if let Ok(repay_ix) = load_instruction_at_checked(len as usize - 1, &ixs) {
+            require_keys_eq!(repay_ix.program_id, ID, ProtocolError::InvalidProgram);
+            require!(repay_ix.data[0..8].eq(instruction::Repay::DISCRIMINATOR), ProtocolError::InvalidIx);
 
+    require_keys_eq!(repay_ix.accounts.get(3).ok_or(ProtocolError::InvalidBorrowerAta)?.pubkey, ctx.accounts.borrower_ata.key(), ProtocolError::InvalidBorrowerAta);
+    require_keys_eq!(repay_ix.accounts.get(4).ok_or(ProtocolError::InvalidProtocolAta)?.pubkey, ctx.accounts.protocol_ata.key(), ProtocolError::InvalidProtocolAta);
+        } else {
+            return Err(ProtocolError::MissingRepayIx.into());
+        }
         Ok(())
     }
-    pub fn repay(ctx:Context<Loan>, borrow_amount:u64) -> Result<()>{
-        Ok(())
+    pub fn repay(ctx:Context<Loan>, _borrow_amount:u64) -> Result<()>{
+        let ixs = ctx.accounts.instructions.to_account_info();
+        let mut amount_borrowed: u64;
+        if let Ok(borrow_ix) = load_instruction_at_checked(0, &ixs) {
+            let mut borrowed_data: [u8;8] = [0u8;8];
+            borrowed_data.copy_from_slice(&borrow_ix.data[8..16]);
+            amount_borrowed = u64::from_le_bytes(borrowed_data);
+        }else {
+            return Err(ProtocolError::MissingBorrowIx.into());
+        }
+
+        let fee = (amount_borrowed as u128).checked_mul(500).unwrap().checked_div(10_000).ok_or(ProtocolError::Overflow)? as u64;
+amount_borrowed = amount_borrowed.checked_add(fee).ok_or(ProtocolError::Overflow)?;
+
+transfer(
+    CpiContext::new(ctx.accounts.token_program.to_account_info(), Transfer {
+        from: ctx.accounts.borrower_ata.to_account_info(),
+        to: ctx.accounts.protocol_ata.to_account_info(),
+        authority: ctx.accounts.borrower.to_account_info(),
+    }), 
+    amount_borrowed
+)?;
+Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Loan<'info> {
-    #[account(&mut)]
+    #[account(mut)]
     pub borrower: Signer<'info>,
 
     #[account(
@@ -38,23 +91,22 @@ pub struct Loan<'info> {
         bump,
     )]
     pub protocol: SystemAccount<'info>,
-
+    pub mint: Account<'info, Mint>,
     #[account(
         init_if_needed,
         payer = borrower,
-        associated_token::mint = mint, 
+        associated_token::mint = mint,
         associated_token::authority = borrower,
-    )]
-    pub borrower_ata: Accounts<'info, TokenAccount>,
-
+      )]
+    pub borrower_ata: Account<'info, TokenAccount>,
     #[account(
         mut, 
         associated_token::authority = protocol,
         associated_token::mint = mint,
     )]
-    pub protocol_ata : Accounts<'info, TokenAccount>,
-
-    #[account(address = INSTRUCTIONS_SYSVAR_ID)]
+    pub protocol_ata : Account<'info, TokenAccount>,
+  /// CHECK: Instructions sysvar account, validated by address constraint
+    #[account(address = INSTRUCTION_SYSVAR_ID)]
     instructions: UncheckedAccount<'info>,
     pub token_program: Program<'info ,Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
